e1a7e3aa7a7b8dc26bec8c970ccaf5e827c40454
evkronos <eliasvieiraresende23@gmail.com>
2025-04-10 10:52:04 -0300

    Added function to get computer choice

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,17 @@
-console.log("Hello")
+function getComputerChoice() {
+    // gerar número aleatório entre 1 e 3
+    let numAleatorio = Math.floor(Math.random() * 3) + 1;
+
+    switch (numAleatorio) {
+        // se número = 1 "pedra"
+        case 1:
+            return console.log("pedra");
+        // se número = 2 "papel"
+        case 2:
+            return console.log("papel");
+        // se número = 3 "tesoura"
+        case 3:
+            return console.log("tesoura");
+    }
+}
+getComputerChoice();
