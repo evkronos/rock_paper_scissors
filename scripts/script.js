2159a09194a6ee092db886ad6b411f8cc0cb9aee
evkronos <eliasvieiraresende23@gmail.com>
2025-04-10 15:10:05 -0300

    Fixed error calling toLowerCase function and fixed error at adding +1 at scores

--- scripts/script.js
+++ scripts/script.js
@@ -23,19 +23,18 @@ function getComputerChoice() {
 function getHumanChoice() {
     // pedir escolha do usuário e armazenar em uma variável
     let humanChoice = prompt("Escolha: pedra, papel ou tesoura");
-    humanChoice.toLowerCase;
+    // converter entrada do usuário para minusculo
+    humanChoice = humanChoice.toLowerCase();
     console.log("Você: " + humanChoice);
     return humanChoice;
 }
 
-// console.log("A sua escolha foi: " + getHumanChoice());
 
-// console.log("A escolha do computador foi: " + getComputerChoice());
 
+// armazenar valores retornados das funções getHumanChoice e getComputerChoice
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
 
-// playRound(humanSelection, computerSelection);
 
 function playGame() {
     // armazenar pontuação do humano 
@@ -45,37 +44,44 @@ function playGame() {
 
 
     function playRound(humanChoice, computerChoice) {
+        humanChoice = getHumanChoice();
+        computerChoice = getComputerChoice();
+
         if (humanChoice === computerChoice) {
             console.log("Empate");
         } else if (humanChoice === "pedra" && computerChoice === "papel") {
-            computerScore = +1;
+            computerScore += 1;
             console.log("Você perdeu! Papel vence pedra");
         } else if (humanChoice === "pedra" && computerChoice === "tesoura") {
-            humanScore = +1;
+            humanScore += 1;
             console.log("Você venceu! Pedra vence tesoura");
         } else if (humanChoice === "papel" && computerChoice === "pedra") {
-            humanScore = +1;
+            humanScore += 1;
             console.log("Você venceu! Papel vence pedra");
         } else if (humanChoice === "papel" && computerChoice === "tesoura") {
-            computerScore = +1;
+            computerScore += 1;
             console.log("Você perdeu! Tesoura vence papel");
         } else if (humanChoice === "tesoura" && computerChoice === "pedra") {
-            computerScore = +1;
+            computerScore += 1;
             console.log("Você perdeu! Pedra vence tesoura");
         } else if (humanChoice === "tesoura" && computerChoice === "papel") {
-            computerScore = +1;
+            computerScore += 1;
             console.log("Você venceu! Tesoura vence papel")
         }
     }
 
-    for (let i = 0; i <= 4; i++) {
+    for (let i = 0; i < 5; i++) {
         playRound(humanSelection, computerSelection);
+        console.log(i);
     }
 
+    function showResult() {
+        console.log("Pontuação")
+        console.log("Humano: " + humanScore)
+        console.log("Computador: " + computerScore)
+    }
 
-    console.log("Pontuação:");
-    console.log("Pontuação:");
-    console.log("Humano: " + humanScore);
-    console.log("Computador: " + computerScore);
+    showResult();
 }
+
 playGame();
