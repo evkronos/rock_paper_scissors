ce9fc5c33ca3b268af3b074ef09ed5352a723555
evkronos <eliasvieiraresende23@gmail.com>
2025-04-10 12:31:12 -0300

    Added function playRound

--- scripts/script.js
+++ scripts/script.js
@@ -17,18 +17,42 @@ function getComputerChoice() {
     }
 }
 
-// obter opção do jogador
+// obter opção do humano
 function getHumanChoice() {
     // pedir escolha do usuário e armazenar em uma variável
     let humanChoice = prompt("Escolha: pedra, papel ou tesoura");
+    humanChoice.toLowerCase;
     return humanChoice;
 }
 
-console.log("A sua escolha foi: " + getHumanChoice());
+// console.log("A sua escolha foi: " + getHumanChoice());
 
-console.log("A escolha do computador foi: " + getComputerChoice());
+// console.log("A escolha do computador foi: " + getComputerChoice());
 
 // armazenar pontuação do humano 
 let humanScore = 0;
 // armazenar pontuação do computador
 let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log("Empate");
+    } else if (humanChoice === "pedra" && computerChoice === "papel") {
+        console.log("Você perdeu! Papel vence pedra");
+    } else if (humanChoice === "pedra" && computerChoice === "tesoura") {
+        console.log("Você venceu! Pedra vence tesoura");
+    } else if (humanChoice === "papel" && computerChoice === "pedra") {
+        console.log("Você venceu! Papel vence pedra");
+    } else if (humanChoice === "papel" && computerChoice === "tesoura") {
+        console.log("Você perdeu! Tesoura vence papel");
+    } else if (humanChoice === "tesoura" && computerChoice === "pedra") {
+        console.log("Você perdeu! Pedra vence tesoura");
+    } else if (humanChoice === "tesoura" && computerChoice === "papel") {
+        console.log("Você venceu! Tesoura vence papel")
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
