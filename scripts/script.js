cdf1c54455e2d8ab8f04b615b6f8435b8bba2edf
evkronos <eliasvieiraresende23@gmail.com>
2025-04-10 13:42:31 -0300

    Added playGame function

--- scripts/script.js
+++ scripts/script.js
@@ -2,18 +2,20 @@
 function getComputerChoice() {
     // gerar número aleatório entre 1 e 3
     let numAleatorio = Math.floor(Math.random() * 3) + 1;
-    let computerChoice = "";
 
     switch (numAleatorio) {
         // se número = 1 "pedra"
         case 1:
-            return computerChoice = "pedra";
+            console.log("Computador: " + "pedra")
+            return "pedra";
         // se número = 2 "papel"
         case 2:
-            return computerChoice = "papel";
+            console.log("Computador: " + "papel")
+            return "papel";
         // se número = 3 "tesoura"
         case 3:
-            return computerChoice = "tesoura";
+            console.log("Computador: " + "tesoura")
+            return "tesoura";
     }
 }
 
@@ -22,6 +24,7 @@ function getHumanChoice() {
     // pedir escolha do usuário e armazenar em uma variável
     let humanChoice = prompt("Escolha: pedra, papel ou tesoura");
     humanChoice.toLowerCase;
+    console.log("Você: " + humanChoice);
     return humanChoice;
 }
 
@@ -29,30 +32,50 @@ function getHumanChoice() {
 
 // console.log("A escolha do computador foi: " + getComputerChoice());
 
-// armazenar pontuação do humano 
-let humanScore = 0;
-// armazenar pontuação do computador
-let computerScore = 0;
-
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice === computerChoice) {
-        console.log("Empate");
-    } else if (humanChoice === "pedra" && computerChoice === "papel") {
-        console.log("Você perdeu! Papel vence pedra");
-    } else if (humanChoice === "pedra" && computerChoice === "tesoura") {
-        console.log("Você venceu! Pedra vence tesoura");
-    } else if (humanChoice === "papel" && computerChoice === "pedra") {
-        console.log("Você venceu! Papel vence pedra");
-    } else if (humanChoice === "papel" && computerChoice === "tesoura") {
-        console.log("Você perdeu! Tesoura vence papel");
-    } else if (humanChoice === "tesoura" && computerChoice === "pedra") {
-        console.log("Você perdeu! Pedra vence tesoura");
-    } else if (humanChoice === "tesoura" && computerChoice === "papel") {
-        console.log("Você venceu! Tesoura vence papel")
-    }
-}
-
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
 
-playRound(humanSelection, computerSelection);
+// playRound(humanSelection, computerSelection);
+
+function playGame() {
+    // armazenar pontuação do humano 
+    let humanScore = 0;
+    // armazenar pontuação do computador
+    let computerScore = 0;
+
+
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice === computerChoice) {
+            console.log("Empate");
+        } else if (humanChoice === "pedra" && computerChoice === "papel") {
+            computerScore = +1;
+            console.log("Você perdeu! Papel vence pedra");
+        } else if (humanChoice === "pedra" && computerChoice === "tesoura") {
+            humanScore = +1;
+            console.log("Você venceu! Pedra vence tesoura");
+        } else if (humanChoice === "papel" && computerChoice === "pedra") {
+            humanScore = +1;
+            console.log("Você venceu! Papel vence pedra");
+        } else if (humanChoice === "papel" && computerChoice === "tesoura") {
+            computerScore = +1;
+            console.log("Você perdeu! Tesoura vence papel");
+        } else if (humanChoice === "tesoura" && computerChoice === "pedra") {
+            computerScore = +1;
+            console.log("Você perdeu! Pedra vence tesoura");
+        } else if (humanChoice === "tesoura" && computerChoice === "papel") {
+            computerScore = +1;
+            console.log("Você venceu! Tesoura vence papel")
+        }
+    }
+
+    for (let i = 0; i <= 4; i++) {
+        playRound(humanSelection, computerSelection);
+    }
+
+
+    console.log("Pontuação:");
+    console.log("Pontuação:");
+    console.log("Humano: " + humanScore);
+    console.log("Computador: " + computerScore);
+}
+playGame();
